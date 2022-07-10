51ed972b3670e85d39525133dd380c8ba09eb6f3
Rocky Kev <Kev.rocky@gmail.com>
2022-07-10 12:55:11 -0700

    feat: made sidebar

--- app.vue
+++ app.vue
@@ -6,10 +6,17 @@
 
     <!-- TODO: Add navbar -->
 
-
-
-    <div class="grid grid-cols-4 gap-4">
-      <div class="board col-span-3 grid grid-cols-5 grid-rows-5 gap-2 p-4">
+    <div class="flex" :class="{ 'show-sidebar': !sidebarOpen }">
+      <div
+        class="
+          board
+          col-span-3
+          grid grid-cols-5 grid-rows-5
+          gap-2
+          p-4
+          duration-300
+        "
+      >
         <BingoCell
           :word="item"
           v-for="(item, index) in words"
@@ -17,12 +24,38 @@
         />
       </div>
 
-      <div class="sidebar">
-      <!-- add shuffle button -->
-      <!-- add a-z button -->
+      <aside class="sidebar flex-shrink-0 relative duration-300 pl-4">
+        <button
+          class="
+            absolute
+            -left-8
+            top-64
+            rotate-90
+            bg-slate-100
+            p-1
+            rounded-sm
+            button-border
+          "
+          @click="sidebarOpen = !sidebarOpen"
+        >
+          Sidebar
+        </button>
+
+        <!-- <div class="h-64 bg-gray-900"></div> -->
+        <!-- <nav class="flex-1 flex flex-col bg-white">
+          <a href="#" class="p-2">Nav Link 1</a>
+          <a href="#" class="p-2">Nav Link 2</a>
+          <a href="#" class="p-2">Nav Link 3</a>
+        </nav> -->
 
         <TextareaField :restored-data="restoredData" @content="getContent" />
-      </div>
+      </aside>
+
+      <!-- <div class="sidebar">
+         add shuffle button -->
+      <!-- add a-z button -->
+
+      <!-- </div>  -->
     </div>
   </main>
 </template>
@@ -33,13 +66,13 @@ import TextareaField from "@/components/TextareaField.vue";
 import PageHeader from "./components/PageHeader.vue";
 import { shuffle, decodeURIToData } from "@/js/shared.js";
 
-
 export default {
   name: "App",
   components: { BingoCell, TextareaField, PageHeader },
   data() {
     return {
       restoredData: "",
+      sidebarOpen: false,
       words: [
         "Lighthouse",
         "Primary color",
@@ -71,9 +104,7 @@ export default {
   },
 
   methods: {
-
     // TODO: Get the url string
-
 
     // shuffle(array) {
     //   let currentIndex = array.length;
@@ -92,33 +123,33 @@ export default {
     //   return array;
     // },
     getContent(value) {
-      console.log("I'm getting the content from the child! Passing it over to this.words")
-      console.log({value})
+      console.log(
+        "I'm getting the content from the child! Passing it over to this.words"
+      );
+      console.log({ value });
       this.words = value;
     },
   },
   created() {
-    console.log("App been mounted", this.words)
+    console.log("App been mounted", this.words);
 
     if (this.$route.query.src) {
-      console.log("data found. Decoding data!")
+      console.log("data found. Decoding data!");
       const uriParam = decodeURIToData(this.$route.query.src);
-       console.log("finished decodeURIToData");
+      console.log("finished decodeURIToData");
 
       // pass this to sidebar
       console.log(uriParam);
       this.restoredData = uriParam;
-      console.log("data successfully restored")
+      console.log("data successfully restored");
 
       // pass this to current data
-      this.words = this.restoredData.data25
+      this.words = this.restoredData.data25;
     }
 
     this.words = shuffle(this.words);
-    console.log("App been shuffled", this.words)
-    console.log(this.$route.query.src)
-
-
+    console.log("App been shuffled", this.words);
+    console.log(this.$route.query.src);
   },
 };
 </script>
@@ -126,6 +157,9 @@ export default {
 <style lang="scss">
 * {
   box-sizing: border-box;
+}
+body {
+  // overflow-x: hidden; /* Hide vertical scrollbar */
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -146,5 +180,29 @@ export default {
   justify-content: space-evenly;
   // gap: 20px;
   // width: 140 * 5px;
+  flex-shrink: 0;
+  flex-basis: 99%;
+}
+
+.sidebar {
+  flex-basis: 1%;
+
+  .button-border {
+    $border: solid lightgray 1px;
+    border-bottom: $border;
+    border-left: $border;
+    border-right: $border;
+  }
+}
+
+.show-sidebar {
+  .board {
+    flex-shrink: 0;
+    flex-basis: 75%;
+  }
+
+  .sidebar {
+    flex-basis: 25%;
+  }
 }
 </style>
